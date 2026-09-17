e5b79a19f62b10fe3f007a0d1480394d9c098231
confsc <syntaxbrawlstars@gmail.com>
2026-09-17 15:43:54 +0300

    Update data_loader.py

--- logic/data_loader.py
+++ logic/data_loader.py
@@ -4,7 +4,6 @@ import sys
 
 
 def resource_path(relative_path):
-    """Возвращает путь к ресурсу — работает и в .exe, и в исходниках."""
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, relative_path)
     return os.path.join(os.path.abspath("."), relative_path)
