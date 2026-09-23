5b6022f05f2e77ba9fb073b755050e0f693c4bd9
confsc <syntaxbrawlstars@gmail.com>
2026-09-23 15:52:36 +0000

    Update paths and gitignore

--- client/logic/data_loader.py
+++ client/logic/data_loader.py
@@ -6,7 +6,8 @@ import sys
 def resource_path(relative_path):
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
+    base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base, relative_path)
 
 
 def load_stations():
